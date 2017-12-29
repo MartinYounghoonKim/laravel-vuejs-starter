1c7e7f411571fe1f56776dd8f37c0ce150ca91c6
martin <martin@within.co.kr>
2017-12-29 11:09:18 +0900

    Update laravel mix config

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,113 @@
 let mix = require('laravel-mix');
+const path = require('path');
+const webpack = require('webpack');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
+/**
+ * @desc Webacpk Javascript 설정
+ */
+mix.js('resources/assets/js/app.js', 'public/js')
+	.extract([
+		'vue',
+		'vuex',
+		'axios',
+		'vee-validate',
+		'velocity-animate'
+	]);
+
+/**
+ * @desc Webacpk Sass 설정
+ */
+mix.sass('resources/assets/sass/app.scss', 'public/css');
+
+/**
+ * @desc Webpak BrowerSync 설정
+ */
+mix.browserSync({ proxy: '127.0.0.1:8000' });
+
+/**
+ * @desc Webacpk Custom Options 설정
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix.webpackConfig({
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				use: [
+					'sass-loader',
+					{
+						loader: 'sass-resources-loader',
+						options: {
+							resources: path.resolve(__dirname, 'resources/assets/sass/helpers/_variables.scss'),
+						},
+					},
+				],
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+				options: {
+					presets: [[
+						'env', {
+							targets: {
+								browsers: ['last 2 versions'],
+							},
+						},
+					]],
+				},
+			},
+			{
+				test: /\.(woff2?|ttf|eot|svg|otf)$/,
+				loader: 'file-loader',
+				options: {
+					name: 'fonts/[name].[ext]?[hash]',
+					publicPath: '/',
+				},
+			},
+		],
+	},
+	resolve: {
+		modules: [path.resolve(__dirname, './resources/assets/'), 'node_modules'],
+		extensions: ['.js', '.vue'],
+		alias: {
+			js: path.resolve(__dirname, './resources/assets/js'),
+			sass: path.resolve(__dirname, './resources/assets/sass'),
+		},
+	},
+	externals: {
+		Daum: 'daum',
+	},
+	plugins: [
+		new webpack.LoaderOptionsPlugin({
+			options: {
+				eslint: {
+					configFile: path.join(__dirname, '.eslintrc'),
+				},
+			},
+		}),
+		new webpack.ProvidePlugin({
+			$: 'jquery',
+			jQuery: 'jquery',
+			'window.jQuery': 'jquery',
+		}),
+	],
+});
+
+/**
+ * @param { Prod/Dev 환경 설정 }
+ * @desc 배포 버전시, 브라우저의 최적화를 위한 JS파일 사이즈 최소화와 캐싱을 위한 버전 라벨링 처리함.
+ */
+if (mix.inProduction()) {
+	//배포 버전시 실행
+	console.log('::::: Webpack Build Production Version :::::');
+	mix
+		.version(['public/js/app.js'])
+		.minify('public/js/app.js');
+}
+else {
+	//개발 버전시 실행
+	mix.sourceMaps();
+	console.log('::::: Webpack Build Development Version :::::');
+}
+;
