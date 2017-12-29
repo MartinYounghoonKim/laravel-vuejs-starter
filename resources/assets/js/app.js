2adcd5ccbff68596b86816d7de73dc7444f8e2d7
martin <martin@within.co.kr>
2017-12-29 11:12:10 +0900

    Update entry files

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,22 +1,65 @@
+/**
+ * @summary appi.js 에 대한 app entry 파일 설정
+ */
+import Vue from 'vue';
+import router from './router';
+import App from './app.vue';
 
 /**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
+ * @summary {bootstrap-vue} 사용
  */
+import bootstrap from './bootstrap';
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm.js';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
-require('./bootstrap');
-
-window.Vue = require('vue');
+let originalVueComponent = Vue.component;
+// 현 Vue-bootstrap 자체 bug fix를 위한 코드 추가
+// 참고: https://github.com/bootstrap-vue/bootstrap-vue/issues/1201
+Vue.component = function (name, definition) {
+	if (name === 'bFormCheckboxGroup' || name === 'bCheckboxGroup' ||
+		name === 'bCheckGroup' || name === 'bFormRadioGroup') {
+		definition.components = { BFormCheckboxGroup: definition.components[0] };
+	}
+	originalVueComponent.apply(this, [name, definition]);
+};
+Vue.use(BootstrapVue);
+Vue.component = originalVueComponent;
+Vue.use(bootstrap);
 
 /**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
+ * @summary {vue-js-toggle-button} Toggle버튼 플러그인 사용
  */
+import ToggleButton from 'vue-js-toggle-button';
 
-Vue.component('example', require('./components/Example.vue'));
+Vue.use(ToggleButton);
 
+/**
+ * @summary { VeeValidate } 사용
+ * @desc global하게 사용되는 validator
+ */
+import ko from 'vee-validate/dist/locale/ko.js';
+
+const config = {
+	locale: 'ko',
+	dictionary: {
+		ko,
+	},
+	fieldsBagName: 'formFields',
+};
+import VeeValidate from 'vee-validate';
+
+Vue.use(VeeValidate, config);
+
+/**
+ * @summary Store 사용
+ */
+import store from './stores/index';
+
+/*eslint-disable-next-line */
 const app = new Vue({
-    el: '#app'
+	el: '#app',
+	router,
+	store,
+	render: h => h(App),
 });
---
+++ resources/assets/js/app.vue
@@ -0,0 +1,18 @@
+<template>
+	<div id="app"></div>
+</template>
+
+<script>
+	export default {
+		name: 'App'
+	};
+</script>
+
+<style lang="scss">
+	@import '../sass/app.scss';
+	#app{
+		position: relative;
+		height: 100%;
+		min-height: 100%;
+	}
+</style>
