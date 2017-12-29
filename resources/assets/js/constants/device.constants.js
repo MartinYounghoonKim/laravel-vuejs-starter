238ec7cb96aa081de97bcfe36a3be429026cfb51
martin <martin@within.co.kr>
2017-12-29 11:21:49 +0900

    Update constants directory with device informations

---
+++ resources/assets/js/constants/device.constants.js
@@ -0,0 +1,16 @@
+/**
+ * @summary { device.constants }
+ * @desc 반응형에 따라 해상도와 디바이스 정보에 대한 constants값 설정
+ */
+
+export const DEVICE_SIZES = {
+	WIDE_PC: '1920',
+	NARROW_PC: '1440',
+	MOBILE: '768'
+};
+
+export const DEVICE_NAMES = {
+	WIDE_PC: 'WIDE_PC',
+	NARROW_PC: 'NARROW_PC',
+	MOBILE: 'MOBILE'
+};
